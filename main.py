6af7cf4502fe0b7e579b0e575a6bc443ee04a3bb
antl-m <tolikmalibroda@gmail.com>
2023-05-01 22:31:06 +0200

    Added txt2gif command;

--- main.py
+++ main.py
@@ -6,12 +6,10 @@ from os import getenv
 from io import BytesIO
 from aiogram.dispatcher.filters.state import StatesGroup, State
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from visuo_gen import txt2img_async
+from aiogram.dispatcher import FSMContext
+from aiogram.types import Message, BotCommand
+from visuo_gen import txt2img_async, txt2gif_async
 
-from aiogram.types import (
-    Message,
-    BotCommand
-)
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -19,43 +17,75 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 class BotState(StatesGroup):
-    waiting_for_prompt = State()
+    txt2img_prompt_wait = State()
+    txt2gif_prompt_wait = State()
     generation_in_progress = State()
 
 async def txt2img_handler(message: Message):
     await message.answer('Input prompt')
-    await BotState.waiting_for_prompt.set()
+    await BotState.txt2img_prompt_wait.set()
 
-async def prompt_handler(message: Message, state: aiogram.dispatcher.FSMContext):
+async def txt2img_prompt_handler(message: Message, state: aiogram.dispatcher.FSMContext):
     try:
         await BotState.generation_in_progress.set()
-        await aiogram.types.ChatActions.upload_photo()
+        await message.answer('Ok, image generation in progress')
         image = await txt2img_async(message.text) 
         bytes = BytesIO()
         image.save(bytes, 'JPEG')
         bytes.seek(0)
-        await message.answer_photo(bytes, message.text)
+        await message.answer_photo(bytes)
     except:
         await message.answer(f'Error occured during generation, try later')
     finally:
         await state.reset_state()
 
-async def echo(message: Message):
-    await message.answer(message.text)
+async def txt2gif_handler(message: Message):
+    await message.answer('Input prompt')
+    await BotState.txt2gif_prompt_wait.set()
+
+async def txt2gif_prompt_handler(message: Message, state: aiogram.dispatcher.FSMContext):
+    try:
+        await BotState.generation_in_progress.set()
+        await message.answer('Ok, animation generation in progress')
+        buf, img = await txt2gif_async(message.text)
+
+        bytes = BytesIO()
+        img.save(bytes, 'JPEG')
+        bytes.seek(0)
+        await message.answer_photo(bytes)
+
+        buf.name = 'image.gif'
+        buf.seek(0)
+        await message.answer_animation(buf)
+    except Exception as ex:
+        await message.answer(f'Error occured during generation, try later:\n{str(ex)}')
+    finally:
+        await state.reset_state()
+    
+async def wait_handler(message: Message):
+    await message.answer("Generation in progress, please wait")
 
 async def main():
     bot = aiogram.Bot(getenv("BOT_TOKEN"))
     await bot.set_my_commands([
-        BotCommand('txt2img', 'Generate image from text')
+        BotCommand('txt2img', 'Generate image from text'),
+        BotCommand('txt2gif', 'Create gif of image generation progress'),
     ])
 
     storage = MemoryStorage()
     dispatcher = aiogram.Dispatcher(bot, storage = storage)
+    
     dispatcher.register_message_handler(txt2img_handler, commands=['txt2img'])
-    dispatcher.register_message_handler(prompt_handler,
-                                        state=BotState.waiting_for_prompt,
+    dispatcher.register_message_handler(txt2img_prompt_handler,
+                                        state=BotState.txt2img_prompt_wait,
                                         content_types=aiogram.types.ContentType.TEXT)
-    dispatcher.register_message_handler(echo, state=BotState.generation_in_progress)
+    
+    dispatcher.register_message_handler(txt2gif_handler, commands=['txt2gif'])
+    dispatcher.register_message_handler(txt2gif_prompt_handler,
+                                        state=BotState.txt2gif_prompt_wait,
+                                        content_types=aiogram.types.ContentType.TEXT)
+    
+    dispatcher.register_message_handler(wait_handler, state=BotState.generation_in_progress)
 
     await dispatcher.start_polling()
 
--- visuo_gen.py
+++ visuo_gen.py
@@ -7,11 +7,6 @@ from torch import FloatTensor
 import asyncio
 import io
 
-def create_gif_cb(pipe: StableDiffusionPipeline, images):
-    def display_cb(step: int, timestep: int, tensor: FloatTensor):
-        images.append(pipe.numpy_to_pil(pipe.decode_latents(tensor))[0])
-    return display_cb
-
 def txt2img(prompt: str) -> Image:
     model_id = 'runwayml/stable-diffusion-v1-5'
     pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
@@ -19,21 +14,29 @@ def txt2img(prompt: str) -> Image:
     image = pipe(prompt, 512, 512, 50).images[0]
     return image
 
-def make_gif(images) -> io.BytesIO:
-    buffer = io.BytesIO()
-    images[0].save(buffer, format='GIF', save_all=True, optimize=True, append_images=images[1:], loop=0)
-    return buffer
-
-def txt2gif(prompt: str) -> io.BytesIO:
-    model_id = 'runwayml/stable-diffusion-v1-5'
-    pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
-    pipe = pipe.to("cuda")
-    images = []
-    pipe(prompt, 512, 512, 50, callback=create_gif_cb(pipe, images))
-    return make_gif(images)
-
 async def txt2img_async(prompt: str) -> Image:
     loop = asyncio.get_running_loop()
     image = await loop.run_in_executor(None, txt2img, prompt)
     return image
-    
+
+def create_gif_cb(pipe: StableDiffusionPipeline, images):
+    def display_cb(step: int, timestep: int, tensor: FloatTensor):
+        images.append(pipe.numpy_to_pil(pipe.decode_latents(tensor))[0])
+    return display_cb
+
+def make_gif(images) -> io.BytesIO:
+    buffer = io.BytesIO()
+    images[0].save(buffer, format='GIF', save_all=True, optimize=True, append_images=images[1:], loop=1, duration=300)
+    return buffer
+
+def txt2gif(prompt: str):
+    model_id = 'runwayml/stable-diffusion-v1-5'
+    pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+    pipe = pipe.to("cuda")
+    images = []
+    pipe(prompt, 512, 512, 50, callback=create_gif_cb(pipe, images), callback_steps=5)
+    return make_gif(images), images[-1]
+
+async def txt2gif_async(prompt: str):
+    loop = asyncio.get_running_loop()
+    return await loop.run_in_executor(None, txt2gif, prompt)
